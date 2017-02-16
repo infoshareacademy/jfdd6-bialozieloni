5e15b1db2a9709a242c2addfb57c1d14a4569240
Bartosz Szajda <bartekszajda@yahoo.com>
2017-02-16 17:08:34 +0100

    wip

--- feature-animations.js
+++ feature-animations.js
@@ -1,7 +1,11 @@
 $(window).on('scroll', function () {
+
+
     var p = $('#functions');
     var offset = p.offset().top;
     var scrollTop = $(window).scrollTop()
-    $( '.func-1-icon, .func-2-icon, .func-3-icon, .func-4-icon' ).addClass( "animate-icons" );
-    console.log(offset, scrollTop);
+    if (scrollTop > (offset - window.outerHeight/2)) {
+        $( '.func-1-icon, .func-2-icon, .func-3-icon, .func-4-icon' ).addClass( "animated flipInY" );
+    }
 });
+
