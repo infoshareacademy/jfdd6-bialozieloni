c305ba9439d7f54a1a03595196d781ca0b20b5ce
Robert Papiernik <robertpapiernik@o2.pl>
2017-02-27 15:28:34 +0100

    Optimization code for team-members photo animation.

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,7 @@
 $(window).on('scroll', function () {
     var p = $('#functions');
     var offset = p.offset().top;
-    var scrollTop = $(window).scrollTop()
+    var scrollTop = $(window).scrollTop();
     if (scrollTop > (offset - window.outerHeight/2)) {
         $( '.func-1-icon, .func-2-icon, .func-3-icon, .func-4-icon' ).addClass( "animated flipInY");
     }
@@ -10,8 +10,8 @@ $(window).on('scroll', function () {
 $(window).on('scroll', function () {
   var p = $('#team');
   var offset = p.offset().top;
-  var scrollTop = $(window).scrollTop()
-  if (scrollTop > (offset - window.outerHeight/2)) {
-    $('.team-member-photo').addClass( "animated rotateIn");
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > offset) {
+    $('.team-member-photo').addClass("animated rotateIn");
   }
 });
